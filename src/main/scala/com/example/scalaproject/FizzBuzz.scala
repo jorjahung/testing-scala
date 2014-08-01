4c2b6eecf47c9b005e579ab841898e9737172f94
Jorja Hung <jorja.hung@springer.com>
2014-08-01 14:46:59 +0100

    extension methods

--- src/main/scala/com/example/scalaproject/FizzBuzz.scala
+++ src/main/scala/com/example/scalaproject/FizzBuzz.scala
@@ -1,19 +1,27 @@
 package com.example.scalaproject
 
 class FizzBuzz {
-  def buzz(x: Int) : Any = {
-    var answer = ""
-    if (divisibleBy(x, 3))
-      answer = "Fizz"
-    if (divisibleBy(x, 5))
-      answer += "Buzz"
-    if (answer == "")
-      x
-    else
-      answer
-   }
+//  def buzz(x: Int) : Any = {
+//    var answer = ""
+//    if (divisibleBy(x, 3))
+//      answer = "Fizz"
+//    if (divisibleBy(x, 5))
+//      answer += "Buzz"
+//    if (answer == "")
+//      x
+//    else
+//      answer
+//   }
 
-  def divisibleBy(x: Int, y: Int): Boolean = {
-    x % y == 0
+
+  def buzz(number: Int) : Any = number match {
+    case x if x divisibleBy 15 => "FizzBuzz"
+    case x if x divisibleBy 3 => "Fizz"
+    case x if x divisibleBy 5 => "Buzz"
+    case _ => number
+  }
+  implicit class RichInt(val self: Int) {
+    def divisibleBy(y: Int): Boolean = self % y == 0
   }
  }
+
